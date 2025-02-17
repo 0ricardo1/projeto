fdee1824a6de5680b0ad5b6a04a48e0d79ff8de4
OM0RI <ricardopalmerense36@gmail.com>
2025-02-17 20:19:07 -0300

    Finalizado

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,12 @@
-function togglemode() {
-    const html = document.documentElement
-
-     html.classList.toggle("light")
-
-     const img = document.querySelector("#profile img")
-
-     if (html.classList.contains("light") ) {
-
-        img.setAttribute("src", "./assets/profile-light.jpg")
-     } else {
-
-        img.setAttribute("src", "./assets/profile-dark.jpg")
-     }
+function toggleMode() {
+   const html = document.documentElement
+   html.classList.toggle('light')
+   
+   const img = document.querySelector("#profile img")
+   
+   if (html.classList.contains("light")) {
+       img.setAttribute("src", "./assets/profile-light.jpg")
+   } else {
+       img.setAttribute("src", "./assets/profile-dark.jpg")
+   }
 }
